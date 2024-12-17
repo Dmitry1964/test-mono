83115d96a45981b1c678701798c2f6d097f6e46b
Dmytry64 <nosdm64@gmail.com>
2024-12-17 17:35:10 +0300

    The End

--- src/js/form.ts
+++ src/js/form.ts
@@ -1,26 +1,43 @@
 import { log } from 'console';
 import IMask from 'imask';
 
-const PHONE_ERROR = 'Введите корректный телефонный номер'
+const PHONE_ERROR = 'Введите корректный телефонный номер';
+const NAME_ERROR = 'Введите имя'
 
 // const template = document.getElementById('popup') as HTMLTemplateElement;
 const phoneField = document.getElementById('phone');
-const phoneContainer = document.querySelector('.info__input-container--phone');
-const nameContainer = document.querySelector('.info__input-container--name');
+const nameField = document.getElementById('name');
+const checkField = document.getElementById('agreement');
+const submitButton = document.querySelector('.info__form-button');
+const phoneContainer = document.querySelector('.info__input-container--phone') as HTMLElement;
+const nameContainer = document.querySelector('.info__input-container--name') as HTMLElement;
 const form = document.getElementById('info-form');
 
-const showErrorPopup = (message: string) => {
+const showErrorPopup = (message: string, elem: HTMLElement) => {
   const template = document.getElementById('popup') as HTMLTemplateElement;
 
   const errorPopup = template.content.cloneNode(true) as HTMLElement;
   errorPopup.querySelector('.popup__message').textContent = message;
-  phoneContainer.append(errorPopup);
+  elem.append(errorPopup);
 }
 
-const removeErrorPopup = () => {
-  const elem = phoneContainer.querySelector('.popup__wrapper');
-  if (elem) {
-    elem.remove()
+const removeErrorPopup = (elem: HTMLElement) => {
+  const popup = elem.querySelector('.popup__wrapper');
+  console.log(popup);
+
+  if (popup) {
+    popup.remove()
+  }
+}
+
+const handleCheckBoxInput = (evt: Event) => {
+  const target = evt.target as HTMLInputElement;
+  if (target.checked) {
+    submitButton.classList.remove('info__form-button--disabled');
+  }
+
+  if (!target.checked) {
+    submitButton.classList.add('info__form-button--disabled');
   }
 }
 
@@ -44,22 +61,42 @@ export const initValidate = () => {
   const regPhone = /^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/;
   const regLetters = /^[A-Za-zА-Яа-яЁё]+$/;
 
-  const handlePhoneField = (evt: FocusEvent) => {
+  const handleInputField = (evt: FocusEvent) => {
     const target = evt.target as HTMLInputElement;
+    const inputName = target.getAttribute('name');
 
+    if (inputName === 'phone') {
       if (!regPhone.test(target.value)) {
         phoneField.classList.add('isError');
-        showErrorPopup(PHONE_ERROR)
+        showErrorPopup(PHONE_ERROR, phoneContainer)
       } else {
-        removeErrorPopup()
+        removeErrorPopup(phoneContainer)
       }
+    }
+
+    if (inputName === 'name') {
+      if (!regLetters.test(target.value)) {
+        nameField.classList.add('isError');
+        showErrorPopup(NAME_ERROR, nameContainer)
+      }
+    } else {
+      removeErrorPopup(nameContainer);
+    }
+
   }
 
-const onChangePhoneField = () => {
+const onChangeInputField = () => {
   phoneField.classList.remove('isError')
+  nameField.classList.remove('isError');
+
+  console.log(nameField);
+
 }
 
-  phoneField.addEventListener('blur', handlePhoneField);
-  phoneField.addEventListener('input', onChangePhoneField )
+  phoneField.addEventListener('blur', handleInputField);
+  phoneField.addEventListener('input', onChangeInputField );
+  nameField.addEventListener('blur', handleInputField);
+  nameField.addEventListener('input', onChangeInputField )
+  checkField.addEventListener('change', handleCheckBoxInput)
   form.addEventListener('submit', aaa);
 }
